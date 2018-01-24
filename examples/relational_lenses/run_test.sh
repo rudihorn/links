b6040d430401e2c0cb68f9f10429982aa9f066c4
Rudi Horn <dyn-git@rudi-horn.de>
2018-01-24 17:04:04 +0000

    fixed missing entries in channelVarUtils

--- examples/relational_lenses/run_test.sh
+++ examples/relational_lenses/run_test.sh
@@ -3,6 +3,6 @@
 psql links < music_example.sql
 echo GRANT ALL ON public.tracks TO links | psql links
 echo GRANT ALL ON public.albums TO links | psql links
-../links cds.links --config=links.config
+../../linx cds.links --config=links.config
 echo "SELECT * FROM albums" | psql links
 echo "SELECT * FROM tracks" | psql links
